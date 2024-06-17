1e09a7335291fa6115969888c80f6440ee18f2c3
lcf33125 <lcf33125@gmail.com>
2024-06-17 16:10:42 +0800

    feat: Add sleep 1

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# 启动 SSH 代理
 eval "$(ssh-agent -s)"
+sleep 1
+
+# 添加 SSH 密钥
 ssh-add ~/.ssh/id_ed25519
+sleep 1
+
+# 列出已添加的 SSH 密钥
 ssh-add -l
+sleep 1
+
+# 测试与 GitHub 的连接
+ssh -T git@github.com
+
